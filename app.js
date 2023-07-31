f3745ab8afc53b5fc1be7d81cf5304c61b950c3a
Muhammad Umar <m.umerpervaiz@gmail.com>
2023-07-31 13:53:54 +0500

    version 0.5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3000;
 
-const version = '0.4';
+const version = '0.5';
 
 app.get('/', (req, res) => {
     // set response content    
