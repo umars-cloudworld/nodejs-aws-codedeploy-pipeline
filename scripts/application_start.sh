363d2477a2f76e997e6a316f3aa04350c0f6c58c
Muhammad Umar <m.umerpervaiz@gmail.com>
2023-07-31 13:31:42 +0500

    scripts modified

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline 
-pm2 restart nodejs-app
+sudo pm2 restart nodejs-app
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline 
-pm2 stop nodejs-app
+sudo pm2 stop nodejs-app
